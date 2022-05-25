a7c4500ff6c3f0969c848ba457157985a6b41481
DoYouPlan <plan1337@yandex.ru>
2022-05-25 20:25:53 +0600

    Добавлен .!.

--- rassada/src/main/java/MyFirstProgram.java
+++ rassada/src/main/java/MyFirstProgram.java
@@ -2,5 +2,22 @@ public class MyFirstProgram {
     public static void main(String[] args){
 
 System.out.println("Hello fucking World!");
+System.out.println("                   ***                 ");
+System.out.println("                  *****                 ");
+System.out.println("                  *****                 ");
+System.out.println("                  *****                  ");
+System.out.println("                  *****                   ");
+System.out.println("                  *****                   ");
+System.out.println("                  *****          ***        ");
+System.out.println("                  *****          ****        ");
+System.out.println("                  *****           *****       ");
+System.out.println("      ****  ****  *****  ******    ******       ");
+System.out.println("    ****** ************ ********   *******      ");
+System.out.println("   ***************************************        ");
+System.out.println("   **************************************        ");
+System.out.println("    ***********************************          ");
+System.out.println("     *******************************           ");
+System.out.println("     ****************************          ");
+
     }
 }
