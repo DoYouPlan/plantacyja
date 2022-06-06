a4342c419b8cdfe0b36c5d61f87f8de75ad0ecac
DoYouPlan <plan1337@yandex.ru>
2022-06-07 03:18:06 +0600

    Добавлена колхозная архитектура

--- rassada/src/test/java/ru/SquareTest.java
+++ rassada/src/test/java/ru/SquareTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class SquareTest {
     @Test
     public void testArea(){
-        Square a = new Square(5);
-        Assert.assertEquals(a.S(),5.0);
+        Square a = new Square(9);
+        Assert.assertEquals(a.S(),2.0);
     }
 }
